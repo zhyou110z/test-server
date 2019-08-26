d2d44a290f2d25f64dd051d7b20346fb3872aff1
Allen <zhy08_25@qq.com>
2019-08-26 17:34:08 +0800

    "group.id","1"    不同GROUP 各自消费   支持订阅功能

---
+++ ms-queue-kafka/src/main/java/org/zhouyou/kafka/Consumer2.java
@@ -0,0 +1,60 @@
+package org.zhouyou.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class Consumer2 {
+
+    static Logger logger = LoggerFactory.getLogger(Consumer2.class);
+
+    private static final String TOPIC = "my_kafka_topic";
+    private static final String BROKER_LIST = "192.168.1.52:9092";
+    private static KafkaConsumer<String,String> consumer = null;
+
+    static {
+        Properties configs = initConfig();
+        consumer = new KafkaConsumer<String, String>(configs);
+    }
+
+    private static Properties initConfig(){
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers",BROKER_LIST);
+        properties.put("group.id","1");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("enable.auto.commit", "true");
+        properties.setProperty("auto.offset.reset", "earliest");
+        return properties;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add(TOPIC);
+        consumer.subscribe(topics);
+//        consumer.subscribe(topics,new ConsumerRebalanceListener() {
+//            @Override
+//            public void onPartitionsRevoked(Collection<TopicPartition> collection) {
+//            }
+//            @Override
+//            public void onPartitionsAssigned(Collection<TopicPartition> collection) {
+//                //将偏移设置到最开始
+//                consumer.seekToBeginning(collection);
+//            }
+//        });
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(10);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, value = %s", record.offset(), record.value());
+                System.out.println();
+            }
+        }
+    }
+}
