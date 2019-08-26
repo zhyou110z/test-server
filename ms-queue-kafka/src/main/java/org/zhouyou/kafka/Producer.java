9dbd88775583a85138c9a447e775ec0aaffb8cc9
Allen <zhy08_25@qq.com>
2019-08-26 16:22:34 +0800

    OPENCV

---
+++ ms-queue-kafka/src/main/java/org/zhouyou/kafka/Producer.java
@@ -0,0 +1,67 @@
+package org.zhouyou.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Producer {
+
+    static Logger log = LoggerFactory.getLogger(Producer.class);
+
+    private static final String TOPIC = "my_kafka_topic";
+    private static final String BROKER_LIST = "192.168.1.52:9092";
+    private static KafkaProducer<String,String> producer = null;
+
+    /*
+    初始化生产者
+     */
+    static {
+        Properties configs = initConfig();
+        producer = new KafkaProducer<String, String>(configs);
+    }
+
+    /*
+    初始化配置
+     */
+    private static Properties initConfig(){
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,BROKER_LIST);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        return properties;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        //消息实体
+        ProducerRecord<String , String> record = null;
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(1000);
+            record = new ProducerRecord<String, String>(TOPIC, "this ms no-"+(int)(10*(Math.random())));
+            //发送消息
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (null != e){
+                        log.info("send error" + e.getMessage());
+                    }else {
+                        System.out.println(String.format("offset:%s,partition:%s",recordMetadata.offset(),recordMetadata.partition()));
+                    }
+                }
+            });
+
+//            //发送消息
+//            producer.send(record, (RecordMetadata recordMetadata, Exception e)->{
+//                    if (null != e){
+//                        log.info("send error" + e.getMessage());
+//                    }else {
+//                        System.out.println(String.format("offset:%s,partition:%s",recordMetadata.offset(),recordMetadata.partition()));
+//                    }
+//            });
+
+        }
+        producer.close();
+    }
+}
