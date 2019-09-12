64afa79f6684aa139396ada24f801ed84db3e50e
Allen <zhy08_25@qq.com>
2019-09-12 16:21:22 +0800

    spring boot admin

--- spring-boot-admin-client/src/main/java/org/zhouyou/AdminClientApplication.java
+++ spring-boot-admin-client/src/main/java/org/zhouyou/AdminClientApplication.java
@@ -2,8 +2,10 @@ package org.zhouyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminClientApplication {
 
     public static void main(String[] args) {
